022db782a06a1c4070e598aa9b4deacf1aa17aa4
JAY DANGAR <jayjaydangar96@gmail.com>
2020-07-23 14:37:47 +0530

    removed the banner

--- lib/main.dart
+++ lib/main.dart
@@ -9,6 +9,7 @@ class MainPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
     return MaterialApp(
+      debugShowCheckedModeBanner: false,
       theme: ThemeData(primarySwatch: Colors.blue),
       home: HomePage(),
     );

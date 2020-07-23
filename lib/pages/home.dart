764a0d1fddfb2e24555bf5abe678613ab9719e01
JAY DANGAR <jayjaydangar96@gmail.com>
2020-07-23 14:36:10 +0530

    Completed the app.

--- lib/pages/home.dart
+++ lib/pages/home.dart
@@ -1,6 +1,4 @@
-import 'package:cube/bloc/slider_bloc.dart';
 import 'package:flutter/material.dart';
-import 'package:flutter_bloc/flutter_bloc.dart';
 
 class HomePage extends StatefulWidget {
   @override
@@ -8,68 +6,78 @@ class HomePage extends StatefulWidget {
 }
 
 class _HomePageState extends State<HomePage> {
+  Offset _offset = Offset(0.5, 0.5);
+  bool switchState = false;
+  double rating = 30;
+  String imageSource = 'lib/assets/frame3.png';
   @override
   Widget build(BuildContext context) {
-    Offset _offset = Offset(0, 0);
-
-    return BlocProvider(
-      create: (context) => SliderBloc(),
-      child: Scaffold(
-        appBar: AppBar(
-          title: Text('Cube'),
-        ),
-        body: Column(
-          children: <Widget>[
-            Expanded(
-              child: BlocBuilder<SliderBloc, SliderState>(
-                  builder: (context, state) {
-                double rating = 30;
-                if (state is SliderChange) {
-                  rating = state.sliderRating;
-                }
-                return Transform(
-                  transform: Matrix4.identity()
-                    ..setEntry(3, 2, 0.001)
-                    ..rotateX(0.01 * _offset.dy)
-                    ..rotateY(-0.01 * _offset.dx),
-                  alignment: FractionalOffset.center,
-                  child: GestureDetector(
-                    onPanUpdate: (details) =>
-                        setState(() => _offset += details.delta),
-                    onDoubleTap: () =>
-                        setState(() => _offset = Offset(0.2, 0.6)),
-                    child: Center(
-                      child: Transform.scale(
-                        scale: rating * 0.02,
-                        child: Image.asset('lib/assets/frame.png'),
-                      ),
-                    ),
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('Cube'),
+      ),
+      body: Column(
+        children: <Widget>[
+          Expanded(
+            flex: 3,
+            child: Transform(
+              transform: Matrix4.identity()
+                ..setEntry(3, 2, 0.001)
+                ..rotateX(0.01 * _offset.dy)
+                ..rotateY(-0.01 * _offset.dx),
+              alignment: FractionalOffset.center,
+              child: Center(
+                child: GestureDetector(
+                  onPanUpdate: (details) {
+                    setState(() => _offset += details.delta);
+                  },
+                  child: Transform.scale(
+                    scale: rating * 0.02,
+                    child: Image.asset(imageSource),
                   ),
-                );
-              }),
-            ),
-            Divider(),
-            Container(
-              height: MediaQuery.of(context).size.height * 0.2,
-              child: BlocBuilder<SliderBloc, SliderState>(
-                builder: (context, state) {
-                  double rating = 30;
-                  if (state is SliderChange) {
-                    rating = state.sliderRating;
-                  }
-                  return Slider(
-                    value: rating,
-                    onChanged: (value) => BlocProvider.of<SliderBloc>(context)
-                        .add(OnChangeSliderEvent(rating: value)),
-                    min: 30,
-                    max: 60,
-                    divisions: 60,
-                  );
-                },
+                ),
               ),
             ),
-          ],
-        ),
+          ),
+          Divider(),
+          Expanded(
+            flex: 1,
+            child: Column(
+              mainAxisAlignment: MainAxisAlignment.spaceBetween,
+              children: [
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceEvenly,
+                  children: <Widget>[
+                    Text('Wire'),
+                    Switch(
+                      value: switchState,
+                      onChanged: (value) => setState(
+                        () {
+                          switchState = value;
+                          if (switchState) {
+                            imageSource = 'lib/assets/frame2.png';
+                          } else {
+                            imageSource = 'lib/assets/frame3.png';
+                          }
+                        },
+                      ),
+                    ),
+                    Text('Solid')
+                  ],
+                ),
+                Slider(
+                  value: rating,
+                  onChanged: (value) => setState(() {
+                    rating = value;
+                  }),
+                  min: 30,
+                  max: 60,
+                  divisions: 60,
+                )
+              ],
+            ),
+          ),
+        ],
       ),
     );
   }
